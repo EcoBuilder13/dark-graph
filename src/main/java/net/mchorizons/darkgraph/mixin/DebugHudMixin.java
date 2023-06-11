03632f3bfb3e00004e11498d9fceb8674d22a887
EcoBuilder13 <68478692+EcoBuilder13@users.noreply.github.com>
2023-06-11 16:22:09 -0400

    Update mixin for 1.20

--- src/main/java/net/mchorizons/darkgraph/mixin/DebugHudMixin.java
+++ src/main/java/net/mchorizons/darkgraph/mixin/DebugHudMixin.java
@@ -13,15 +13,13 @@ public class DebugHudMixin {
 
     //background color: -1873784752
 
-    //horizontal lines
-    @ModifyArg(method="drawMetricsData", at=@At(value="INVOKE", target="Lnet/minecraft/client/gui/hud/DebugHud;drawHorizontalLine(Lnet/minecraft/client/util/math/MatrixStack;IIII)V"), index=4)
-    private int modifyHorizontal(int e) {
+    @ModifyArg(method="drawMetricsData", at=@At(value="INVOKE", target="Lnet/minecraft/client/gui/DrawContext;drawHorizontalLine(Lnet/minecraft/client/render/RenderLayer;IIII)V"), index=4)
+    private int drawHorizontalLine(int e) {
         return -1873784742;
     }
 
-    //vertical lines
-    @ModifyArg(method="drawMetricsData", at=@At(value="INVOKE", target="Lnet/minecraft/client/gui/hud/DebugHud;drawVerticalLine(Lnet/minecraft/client/util/math/MatrixStack;IIII)V"), index=4)
-    private int modifyVertical(int e) {
+    @ModifyArg(method="drawMetricsData", at=@At(value="INVOKE", target="Lnet/minecraft/client/gui/DrawContext;drawVerticalLine(Lnet/minecraft/client/render/RenderLayer;IIII)V"), index=4)
+    private int drawVerticalLine(int e) {
         return -1873784742;
     }
 }
